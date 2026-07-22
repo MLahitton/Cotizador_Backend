5dbf170659216521c15e67f75f7cf01b1169e9ea
MLahitton <222233533+MLahitton@users.noreply.github.com>
2026-07-22 16:34:03 -0500

    feat: add client creator migration

---
+++ Infrastructure/Persistence/Migrations/20260722211905_AddClientCreatedByUser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClientCreatedByUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "created_by_user_id",
+                schema: "core",
+                table: "clients",
+                type: "uuid",
+                nullable: false);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_clients_created_by_user_id",
+                schema: "core",
+                table: "clients",
+                column: "created_by_user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_clients_users_created_by_user_id",
+                schema: "core",
+                table: "clients",
+                column: "created_by_user_id",
+                principalSchema: "identity",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_clients_users_created_by_user_id",
+                schema: "core",
+                table: "clients");
+
+            migrationBuilder.DropIndex(
+                name: "ix_clients_created_by_user_id",
+                schema: "core",
+                table: "clients");
+
+            migrationBuilder.DropColumn(
+                name: "created_by_user_id",
+                schema: "core",
+                table: "clients");
+        }
+    }
+}
